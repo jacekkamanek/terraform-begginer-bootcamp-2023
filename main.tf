940957096b7c784457849d9c1adb6e5d13b400c7
jacekkamanek vbfbfb <bw61180@gmail.com>
2023-10-18 11:47:12 +0000

    12

--- main.tf
+++ main.tf
@@ -7,29 +7,29 @@ terraform {
   }
 }
 
-  resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
+#   resource "aws_vpc" "my_vpc" {
+#   cidr_block = "172.16.0.0/16"
 
-  tags = {
-    Name = "tf-example"
-  }
-}
+#   tags = {
+#     Name = "tf-example"
+#   }
+# }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "eu-central-1a"
+# resource "aws_subnet" "my_subnet" {
+#   vpc_id            = aws_vpc.my_vpc.id
+#   cidr_block        = "172.16.10.0/24"
+#   availability_zone = "eu-central-1a"
 
-  tags = {
-    Name = "tf-example"
-  }
-}
+#   tags = {
+#     Name = "tf-example"
+#   }
+# }
 
-resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
+# resource "aws_network_interface" "foo" {
+#   subnet_id   = aws_subnet.my_subnet.id
+#   private_ips = ["172.16.10.100"]
 
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
+#   tags = {
+#     Name = "primary_network_interface"
+#   }
+# }
